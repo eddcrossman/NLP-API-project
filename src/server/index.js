fea72055dbb7540e408c09b6f03161368f3eefd5
Edd Crossman <eddcrossman@hotmail.com>
2023-03-07 16:35:10 +0000

    added service workers

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,18 @@
-var path = require('path')
-const express = require('express')
-//const mockAPIResponse = require('./mockAPI.js')
+const path = require('path');
+const express = require('express');
+const app = express()
+app.use(express.static('dist'))
+console.log(__dirname)
+
+//Fetch and FormData to make request from MeaningCloud API
 const fetch = require('node-fetch');
-var FormData = require('form-data');
+const FormData = require('form-data');
 
-
-
+//Dotenv to hide API key
 const dotenv = require('dotenv');
 dotenv.config();
 
-const app = express()
-
+//BodyParser for incoming JSON
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -19,29 +21,15 @@ app.use(bodyParser.json());
 const cors = require('cors');
 app.use(cors());
 
-app.use(express.static('dist'))
-
-console.log(__dirname)
-
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
-
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
-    console.log('Example app listening on port 8081!')
+    console.log('Listening on port 8081!')
 })
 
-// app.get('/test', function (req, res) {
-//     console.log('/test called');
-//     res.send(mockAPIResponse)
-// })
-
-// app.get('/getMeaningAPIKey', (req, res)=> {
-//     console.log('/getMeaningAPIKey called');
-//     res.send({ key: process.env.API_KEY });
-// })
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html');
+    //res.sendFile(path.resolve('src/client/views/index.html'))
+})
 
 app.post('/getMeaning', async (req, res)=> {
     console.log('/getMeaning called');
